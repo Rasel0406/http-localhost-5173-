3aedbce545f49f8a5cd4ba06458c8a99cdee7711
Rasl0406 <durjoychowdhury0406@gmail.comm>
2026-03-04 16:31:58 +0600

    json data loaded and customer ticket card create

--- ticket-system/src/App.jsx
+++ ticket-system/src/App.jsx
@@ -1,54 +1,28 @@
-import { useState } from 'react'
+
 import './App.css'
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import MainSection from './components/main_section';
+import { Suspense } from 'react';
 
+// fetch data
+const fetchData = async () => {
+  const response =await fetch('../public/ticket.json');
+  const data = response.json();
+  return data;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+const fetchDataResult = fetchData();
 
   return (
     <>
-      <Navbar>  </Navbar>
+      <Navbar></Navbar>
       <Banner></Banner>
-      {/* <div className="navbar bg-base-100 shadow-sm ">
-        <div className=" flex container mx-auto px-4">
-          <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a>Home</a></li>
-              <li><a>Changelog</a></li>
-              <li><a>Blog</a></li>
-              <li><a>Download</a></li>
-              <li><a>Contact</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">CS — Ticket System</a>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-lg">
-            <li><a>Home</a></li>
-            <li><a>Changelog</a></li>
-            <li><a>Blog</a></li>
-            <li><a>Download</a></li>
-            <li><a>Contact</a></li>
-          </ul>
-           <div>
-       <button className="flex items-center gap-2 bg-gradient-to-r from-purple-600 to-indigo-600 text-white px-4 py-2 rounded-lg text-sm font-medium shadow-md hover:shadow-lg hover:scale-105 transition-all duration-200">
-  <FaPlus className="text-xs" />
-  New Ticket
-</button>
-        </div>
-        </div>
-
-        </div>
-       
-      </div> */}
+    <Suspense>
+        <MainSection fetchDataResult={fetchDataResult}></MainSection>
+    </Suspense>
+    
     </>
   )
 }
---
+++ ticket-system/src/components/main_section.jsx
@@ -0,0 +1,67 @@
+import React, { use } from 'react';
+
+const Main_section = ({ fetchDataResult }) => {
+    const ticketData = use(fetchDataResult)
+    console.log(ticketData);
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+
+                {/* Left Side - Customer Tickets (2/3 width) */}
+                <div className="lg:col-span-2">
+                    <h2 className="text-2xl font-bold mb-6">Customer Tickets</h2>
+
+                    {/* Cards Grid - 2 columns */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        {/* Single Ticket Card */}
+
+
+                        {
+                            ticketData.map(ticket => (
+                                <div className="bg-white border-2  border-white rounded-lg p-6 shadow-xl">
+                                    <div className="flex justify-between items-start mb-2">
+                                        <h3 className="font-bold text-base">{ticket.title}</h3>
+                                        <div>
+                                            <span className={`${ticket.status === 'Open' ? 'bg-[#B9F8CF] text-[#0B5E06]' : 'bg-[#FEF3C7] text-[#92400E]'} text-md font-bold px-6 py-2 rounded-full flex items-center gap-2`}>
+                                                <span className={`w-4 h-4 ${ticket.status === 'Open' ? 'bg-[#02A53B]' : 'bg-[#F59E0B]'} rounded-full`}></span>
+                                                {ticket.status}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <p className="text-sm text-gray-600 mb-3">{ticket.description}</p>
+                                    <div className="flex justify-between items-center text-sm text-gray-500">
+                                        <div className="font-semibold">
+                                            <span className="text-gray-600 ">#100{ticket.id}</span>
+                                            <span className={`ml-6 ${ticket.priority === 'High' ? 'text-red-600' :
+                                                    ticket.priority === 'Medium' ? 'text-orange-500' :
+                                                        'text-blue-500'
+                                                }`}>
+                                                {ticket.priority.toUpperCase()} PRIORITY
+                                            </span>
+
+                                        </div>
+                                        <div className="flex space-x-4 font-semibold">
+                                            <span>{ticket.customer}</span>
+                                            <span>{ticket.createdAt}</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+
+                {/* Right Side - Task Status (1/3 width) */}
+                <div className="lg:col-span-1 ml-10">
+                    <h2 className="text-2xl font-bold mb-6">Task Status</h2>
+                    {/* Task list */}
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Main_section;
